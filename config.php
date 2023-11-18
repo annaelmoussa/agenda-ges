cedf45126c96ec16364295224418be26a31d36a1
annael <annaelmoussa@gmail.com>
2023-11-18 14:09:17 +0100

    Add .vscode/ and .env.* files to .gitignore, and update composer.json and config.php

--- config.php
+++ config.php
@@ -1,6 +1,15 @@
 <?php
 
+require_once 'vendor/autoload.php';
+
+// Charge les variables d'environnement
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
 // Constant name can be the firstname of the user
 // Exemple : define('MATTHIEU', array('MYGES_USERNAME' => 'CHANGEME', 'MYGES_PASSWORD' => 'CHANGEME'));
-define('CHANGEME', array('MYGES_USERNAME' => 'CHANGEME', 'MYGES_PASSWORD' => 'CHANGEME'));
+define('CHANGEME', array(
+    'MYGES_USERNAME' => $_ENV['MYGES_USERNAME'], 
+    'MYGES_PASSWORD' => $_ENV['MYGES_PASSWORD']
+));
 
